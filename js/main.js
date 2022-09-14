0a6c9f23d0ce1fe33d6d3560e53024348749a76b
Khang Nguyen <nguyentruongkhang.1509@gmail.com>
2022-09-14 17:14:43 +0700

    add function checkGameStatus

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-import { TURN } from "./constants.js";
+import { CELL_VALUE, GAME_STATUS, TURN } from "./constants.js";
 import {
   getCellElementAtIdx,
   getCellElementList,
   getCurrentTurnElement,
   getGameStatusElement,
+  getReplayButtonElement,
 } from "./selectors.js";
+import { checkGameStatus } from "./utils.js";
 
+// console.log(checkGameStatus(["X", "O", "X", "X", "O", "X", "", "X", "O"]));
 /**
  * Global variables
  */
 let currentTurn = TURN.CROSS;
-let isGameEnded = false;
+let gameStatus = GAME_STATUS.PLAYING;
 let cellValues = new Array(9).fill("");
 
 function toggleTurn() {
@@ -23,15 +26,74 @@ function toggleTurn() {
   }
 }
 
+function updateGameStatus(newStatus) {
+  gameStatus = newStatus;
+  const gameStatusElement = getGameStatusElement();
+  if (gameStatusElement) {
+    gameStatusElement.textContent = newStatus;
+  }
+}
+
+function showReplayButton() {
+  const replayButton = getReplayButtonElement();
+  if (replayButton) {
+    replayButton.classList.add("show");
+  }
+}
+
+function hightlightWinCells(winPositions) {
+  if (!Array.isArray(winPositions) || winPositions.length !== 3) {
+    throw new Error("Invalid win positions");
+  }
+
+  for (const position of winPositions) {
+    const cell = getCellElementAtIdx(position);
+    cell.classList.add("win");
+  }
+}
+
 function handleCellClick(cell, index) {
   const isClicked =
     cell.classList.contains(TURN.CIRCLE) || cell.classList.contains(TURN.CROSS);
-  if (isClicked) return;
+  if (isClicked || gameStatus !== GAME_STATUS.PLAYING) return;
+
   // set selected cell
   cell.classList.add(currentTurn);
 
+  // update cell values
+  cellValues[index] =
+    currentTurn === TURN.CIRCLE ? CELL_VALUE.CIRCLE : CELL_VALUE.CROSS;
+
   // toggle turn
   toggleTurn();
+
+  // check game status
+  const game = checkGameStatus(cellValues);
+  switch (game.status) {
+    case GAME_STATUS.ENDED: {
+      // update game status
+      updateGameStatus(game.status);
+      // show replay button
+      showReplayButton();
+      break;
+    }
+
+    case GAME_STATUS.X_WIN:
+    case GAME_STATUS.O_WIN: {
+      // update game status
+      updateGameStatus(game.status);
+      // show replay button
+      showReplayButton();
+      // hightlight win cells
+      hightlightWinCells(game.winPositions);
+      break;
+    }
+
+    default:
+      // playing
+      break;
+  }
+
   console.log("cell", cell, index);
 }
 
